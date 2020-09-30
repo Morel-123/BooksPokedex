7387539620659a3474bffa19527c1bb33e80d003
https://orelsmail@gmail.com@github.com <orelsmail@gmail.com>
2020-09-30 11:47:24 +0300

    - Added constants for firebase and components

---
+++ src/constants/ComponentsConstants.js
@@ -0,0 +1,8 @@
+ // Props names
+
+ /** Is the user registering with an email and a password or authenticated with gmail/facebook
+  * @Type boolean
+  */
+const isPasswordRegistration = "isPasswordRegistration"
+
+ // Props names
---
+++ src/constants/FirebaseConstants.js
@@ -0,0 +1,3 @@
+export const COLLECTION_BOOKS = 'books';
+export const COLLECTION_USERS = 'users';
+export const COLLECTION_GENRES = 'genres';
