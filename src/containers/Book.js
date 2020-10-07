2c76c3e9bc31d897f120ec25069990363841132e
Morel-123 <morelsmail123>
2020-10-07 13:53:19 +0300

    add extra info container + description

--- src/containers/Book.js
+++ src/containers/Book.js
@@ -6,9 +6,14 @@ import {
   StyleSheet,
   Button,
   Image,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
 } from "react-native";
 import { firebase } from "../firebase/Config";
 import { useDispatch, useSelector } from "react-redux";
+import ReadMore from "react-native-read-more-text";
+import { TouchableRipple } from "react-native-paper";
 
 function Book(props) {
   const dispatch = useDispatch();
@@ -21,57 +26,130 @@ function Book(props) {
     props.navigation.navigate("My Books");
   };
 
+  const renderTruncatedFooter = (handlePress) => {
+    return (
+      <TouchableRipple
+        rippleColor="rgba(0, 0, 0, .32)"
+        style={{ alignSelf: "flex-start" }}
+        onPress={handlePress}
+      >
+        <Text style={{ color: "#448aff", marginTop: 0 }}>Show more</Text>
+      </TouchableRipple>
+    );
+  };
+
+  const renderRevealedFooter = (handlePress) => {
+    return (
+      <TouchableRipple
+        rippleColor="rgba(0, 0, 0, .32)"
+        style={{ alignSelf: "flex-start" }}
+        onPress={handlePress}
+      >
+        <Text style={{ color: "#448aff", marginTop: 0 }}>Show less</Text>
+      </TouchableRipple>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      {/* <Text>Book Info</Text> */}
-      <View style={{ display: "flex", flexDirection: "row", width: "100%" }}>
-        <Image
-          style={{ height: 220, width: 150, marginLeft: 10, marginRight: 5 }}
-          source={{
-            uri: book.imageLinks.thumbnail,
-          }}
-          resizeMode="stretch"
-        />
-        <View style={{ display: "flex", width: "55%" }}>
-          <Text style={{ fontSize: 24, textAlign: "center", color: "#212121", fontWeight: "bold" }}>
-            {book.title}
-          </Text>
-          <Text style={{ fontSize: 16, textAlign: "center", color: "#757575" }}>
-            {book.authors[0]}
-          </Text>
-        </View>
-      </View>
-      <View style={styles.extraInfoWrapper}>
-        <View style={styles.extraInfoContainer}>
-          <View style={styles.extraInfoDetailContainer}>
-            <Text style={[styles.extraInfoDetailText, styles.extraInfoDetailTitle]}>Genre</Text>
-            <Text style={[styles.extraInfoDetailText, styles.extraInfoDetailValue]}>{book.categories[0]}</Text>
-          </View>
-          <View style={styles.extraInfoDetailContainer}>
-            <Text style={[styles.extraInfoDetailText, styles.extraInfoDetailTitle]}>Pages</Text>
-            <Text style={[styles.extraInfoDetailText, styles.extraInfoDetailValue]}>{book.pageCount}</Text>
-          </View>
-          <View style={styles.extraInfoDetailContainer}>
-            <Text style={[styles.extraInfoDetailText, styles.extraInfoDetailTitle]}>Released</Text>
-            <Text style={[styles.extraInfoDetailText, styles.extraInfoDetailValue]}>{book.publishedDate.substring(0, 4)}</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+          <Image
+            style={{ height: 220, width: 150, marginLeft: 10, marginRight: 5 }}
+            source={{
+              uri: book.imageLinks.thumbnail,
+            }}
+            resizeMode="stretch"
+          />
+          <View style={{ display: "flex", width: "55%" }}>
+            <Text
+              style={{
+                fontSize: 24,
+                textAlign: "center",
+                color: "#212121",
+                fontWeight: "bold",
+              }}
+            >
+              {book.title}
+            </Text>
+            <Text
+              style={{ fontSize: 16, textAlign: "center", color: "#757575", marginTop: 5 }}
+            >
+              {book.authors[0]}
+            </Text>
           </View>
         </View>
+        <View style={styles.extraInfoWrapper}>
+          <View style={styles.extraInfoContainer}>
+            <View style={styles.extraInfoDetailContainer}>
+              <Text
+                style={[
+                  styles.extraInfoDetailText,
+                  styles.extraInfoDetailTitle,
+                ]}
+              >
+                Genre
+              </Text>
+              <Text
+                style={[
+                  styles.extraInfoDetailText,
+                  styles.extraInfoDetailValue,
+                ]}
+              >
+                {book.categories[0]}
+              </Text>
+            </View>
+            <View style={styles.extraInfoDetailContainer}>
+              <Text
+                style={[
+                  styles.extraInfoDetailText,
+                  styles.extraInfoDetailTitle,
+                ]}
+              >
+                Pages
+              </Text>
+              <Text
+                style={[
+                  styles.extraInfoDetailText,
+                  styles.extraInfoDetailValue,
+                ]}
+              >
+                {book.pageCount}
+              </Text>
+            </View>
+            <View style={styles.extraInfoDetailContainer}>
+              <Text
+                style={[
+                  styles.extraInfoDetailText,
+                  styles.extraInfoDetailTitle,
+                ]}
+              >
+                Released
+              </Text>
+              <Text
+                style={[
+                  styles.extraInfoDetailText,
+                  styles.extraInfoDetailValue,
+                ]}
+              >
+                {book.publishedDate.substring(0, 4)}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ marginTop: 5, marginLeft: 7, width: "100%" }}>
+          <Text style={{ fontSize: 20 }}>Description</Text>
+          <ReadMore
+            numberOfLines={3}
+            renderTruncatedFooter={renderTruncatedFooter}
+            renderRevealedFooter={renderRevealedFooter}
+          >
+            <Text style={{ fontSize: 16 }}>{book.description}</Text>
+          </ReadMore>
+        </View>
+        <Button title="Go Back" onPress={handleBack} />
       </View>
-      {/* <Text>{book.authors[0]}</Text> */}
-      {/* <Text>{book.description}</Text> */}
-      {/* <Text>{book.averageRating}</Text> */}
-      {/* <Text>{book.categories[0]}</Text> */}
-      {/* <Text>{book.pageCount}</Text> */}
-      {/* <Text>{book.publishedDate.substring(0, 4)}</Text> */}
-      {/* <Image
-        style={{ height: 200, width: 200 }}
-        source={{
-          uri: book.imageLinks.thumbnail,
-        }}
-        resizeMode="contain"
-      /> */}
-      <Button title="Go Back" onPress={handleBack} />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -82,15 +160,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
   },
   extraInfoWrapper: {
     width: "100%",
-    height: "12%",
+    // height: "12%",
+    height: Dimensions.get("window").height * 0.1,
     display: "flex",
     alignItems: "center",
   },
   extraInfoContainer: {
-    width: "95%",
+    width: "97%",
     height: "100%",
     backgroundColor: "#f44336",
     borderTopLeftRadius: 15,
@@ -114,10 +197,10 @@ const styles = StyleSheet.create({
   },
   extraInfoDetailTitle: {
     fontSize: 16,
-    color: "white"
+    color: "white",
   },
   extraInfoDetailValue: {
     fontSize: 20,
     fontWeight: "bold",
-  }
+  },
 });
